f479e1037c22dc977772ec929a91b388ca428e08
codewalker3 <gustavootaku2022@gmail.com>
2023-01-14 15:11:13 -0300

    Corrigindo nome de arquivos de configurações

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AgroGS.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AgroGs.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
