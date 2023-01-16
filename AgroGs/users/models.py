10cbc855a4fd37d4c94f50e4716346a3b38c79bd
codewalker3 <gustavootaku2022@gmail.com>
2023-01-16 20:23:55 -0300

    Editando Vendors e adicionando Address

--- AgroGs/users/models.py
+++ AgroGs/users/models.py
@@ -1,27 +1,48 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
-
+class Address(models.Model):
+    city = models.CharField(
+        verbose_name="City",
+        max_length=100,
+        null=False, blank=False
+    )
+    state = models.CharField(
+        verbose_name="Name",
+        max_length=100,
+        null=False, blank=False
+    )
+    phone = models.CharField(
+        verbose_name="Name",
+        max_length=100,
+        null=True, blank=True
+    )
+    street = models.CharField(
+        verbose_name="Name",
+        max_length=100,
+        null=False, blank=False
+    )
+    number = models.IntegerField(
+        verbose_name="Number",
+        null=False, blank=False
+    )
+    compliment = models.CharField(
+        verbose_name="Compliment",
+        max_length=100,
+        null=True, blank=True
+    )
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE   
+        )
 class Vendor(models.Model):
     
     user = models.OneToOneField(
 		User, 
 		on_delete=models.CASCADE
 	)
-    name = models.CharField(
-        verbose_name="Name",
-        max_length=100,
-        null=False, blank=False
-    )
-    email = models.EmailField(
-        verbose_name="E-mail",
-        max_length=200,
-        null=False, blank=False
-    )
-    phone = models.CharField(
-        verbose_name="Phone",
-        max_length=100,
-        null=True, blank=True
+    is_vendor = models.BooleanField(
+        default=False
     )
     cnpj = models.CharField(
         verbose_name="Vendor",
